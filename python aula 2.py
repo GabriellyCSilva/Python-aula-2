58cd4d1782a1b444a69f93952b91d0205b25958a
Gabrielly Candido <gabriellycasilva@gmail.com>
2024-09-12 21:00:18 -0300

    Add files via upload

---
+++ python aula 2.py
@@ -0,0 +1,46 @@
+#a = 10
+#b = 2
+#print('a novo:', a, '/', 'b novo', b)
+#print()
+#aux= a
+#a=b
+#b= aux
+
+# a= 20
+# b=30
+# c=35
+#
+# soma = a+ b + c
+# print(soma)
+#
+# dia= 1
+# horas= 3
+# minuto= 46
+# segundo= 40
+#
+# segundos= ( minuto*60) + (horas*3600) + (dia*86400) + (segundo)
+# print(segundos)
+
+# SeT= 87426
+# x=3600
+# horas= SeT//x
+# print(horas)
+# Minutos= (SeT%x)//60
+# print(Minutos)
+# Segundo= (SeT%x)%60
+# print(Segundo)
+
+# Minuto= 60//60
+# print(Minuto)
+# Segundo= 60%60
+# print(Segundo)
+
+
+#ou
+# seg_tot= 5000
+# segf= seg_tot % 60
+# min = seg_tot//60
+# minf= min%60
+# hora=min//60
+#
+# print(seg_tot, 'segundos totais', '=', hora, 'hora', minf, 'min', segf, 'seg')
