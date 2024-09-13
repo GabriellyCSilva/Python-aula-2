ce59c729b3fa5741ef3abe366f730399b9052ab4
Gabrielly Candido <gabriellycasilva@gmail.com>
2024-09-12 22:52:29 -0300

    Add files via upload

---
+++ Bool_aula2_part2.py
@@ -0,0 +1,49 @@
+#bool
+# Verdadeiro= True
+# Falso= False
+#type-para ver o tipo da variável
+
+#3+4 == 7
+# print(3+4 == 7)
+
+# a= 3+4 == 7
+# print(a)
+
+# 5.0!=5 #ele considera a mesma coisa, mesmo sendo tipos diferentes
+# print(5.0!=5)
+
+#True= passei / False= Reprovei
+# Media= 6
+# CP1= 8
+# CP2= 5
+# CP3= 7
+# GS=7
+
+# NotaF= (CP1+CP2+CP3+GS)/4
+# Media<= (NotaF)
+# print(6<= (NotaF)) se quiser ver a nota final= print NotaF, NotaF
+
+
+#Precisa pagar imposto caso seja maior que 1200
+# Salario= 900
+#print(Salario>1200)
+
+# 5*4<4+3    20<7  - False
+#6*2-1>3*1   11>3  - True
+#9-4/2<=7+1 ou 5*2-3!=6
+#7<8  ou  7!=6
+#False  or  True= True
+
+# idade=17
+# salario= 50000
+
+# idade>=18 and salario>=10000
+
+# print(idade>=18 and salario>=10000)
+
+# idade=17
+# salario= 50000
+# flag= 34
+#exp= idade>=18 and salario>=10000 or flag ==34
+# print(exp)
+
