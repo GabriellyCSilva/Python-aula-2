d68bc232a677400a7e5e663bbf2d351eea1e6ce0
Gabrielly Candido <gabriellycasilva@gmail.com>
2024-09-19 22:44:02 -0300

    Add files via upload

---
+++ Python aula 3.py
@@ -0,0 +1,113 @@
+
+
+
+#nomedeusuario= input('Nome de usuário:')
+#escrever o nome na frente do nome de usuario e dar enter
+#idade= input('Digite sua idade:')
+#print('meu nome é', nomedeusuario, ' e tenho', idade , 'anos')
+
+#n1= int(input('Digite o n1:'))  #para somar os numeros, precisamos colocar o int, caso não coleque, concatena
+#n2= int(input('Digite o n2:'))  #float usamos para calcular com numeros que não são inteiros
+#print(n1+n2)
+
+#valorporano= 1000
+#anos= 7
+#print('O valor do bonus em', anos , 'é' , valorporano*anos)
+
+#metros= 2000
+#print('O valor de', metros, 'metros em milímetros é', metros*1000) #decorar a ordem
+
+
+
+
+
+
+
+
+
+#distancia= int(input('Distancia: '))
+#velociada= int(input('Velocidade:'))
+#tempo= distancia / velociada
+#print('o tempo da viagem foi de:', tempo, 'horas')
+
+#Dia= int(input('Dias alugados:'))
+#kmrodado= int(input('KM rodado:'))
+
+#preçopordia= 60
+#valordokm= 0.15
+#print('O valor a pagar é de', Dia*preçopordia+kmrodado*valordokm)
+
+#n1= int(input('Digite o numero 1:'))
+#n2= int(input('Digite o numero 2:'))
+#res= (n1%2==0 and n2%2==1) or (n1%2==1 and n2%2==0)
+#res = n1%2 != n2%2
+#(res)
+
+##IF
+
+#n1= 4
+#n3= 5
+#if n3>n1 : print(True)
+
+#n1= int(input('Digite numero 1:'))
+#n2= int(input('Digite numero 2:'))
+
+#if n1>n2 :
+#    print(n1, 'é maior que', n2)
+#if n2>n1 :
+#    print(n2, 'é maior que', n1)
+
+#velocidade= int(input('Digita a velocidade:'))
+#limitedevelocidade= 80
+#valordokm=5
+#if velocidade > limitedevelocidade :
+#     print('Você foi multado em,' , (velocidade-limitedevelocidade)*5)
+#if velocidade< limitedevelocidade :
+#     print('Ok')
+
+#a= 30
+#b=70
+
+#if a>b:
+#     print(' b é menor ')
+#else:
+#     print('b é maior')
+
+
+#Façam um programa que print em tele se o cliente pode realizar um emprestimo, baseado em seu salario, idade.
+#Salário>1500
+#idade>Maior
+
+
+#idade= int(input('Sua idade:'))
+#salário= int(input('Seu salário:'))
+
+#salariom= 1500
+#idadem= 18
+
+#if salário>salariom and idade>idadem:
+#     print('Voce está apto')
+#else:
+#     print('Viva ou ganhe mais')
+
+#if salário> salariom:
+#     if idade> idadem:
+#          print('Você está apto')
+#     else:
+#          print('Não está apto')
+#     else:
+#print('Não está apto')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
