e51c9ec16f2e9b09d7e7716febeda365fa785ac9
Gabrielly Candido <gabriellycasilva@gmail.com>
2024-10-03 22:32:52 -0300

    Add files via upload

---
+++ pythonProject/aula03/10.py
@@ -0,0 +1,78 @@
+"""x= float(input('Insira o valor do x='))
+
+if x>= 0 and x<=2:
+    y=x
+elif x > 2 and x <= 3.5:
+    y=2
+elif x > 3.5 and x <=5:
+    y=3
+elif x> 5:
+    y= x**2 - 10*x + 28
+
+print(y)
+"""
+
+"""idade=18
+nome='Maria'
+txt= "%s tem %d anos" % (nome, idade)
+print(txt)"""
+
+"""lanche=input('Insira seu lanche favorito:')
+preço= float(input('Insira o preço:'))
+idade=int(input("Digita sua idade:"))
+nome=input('Insira seu nome:')
+
+txt= '%s gosta de %s, que custa %d e ela tem %d'  % (nome, lanche , preço , idade)
+
+txt= "{} tem {} e seu lanche custa {}".format(*args: nome , idade, preço)
+print(txt)"""
+
+"""print(f"{'':-^18s}")
+print("uva" , 'R$10')
+print(f"{'':-^18s}")
+print("morango" , 'R$10')
+print(f"{'':-^18s}")
+print("limão" , 'R$10')
+print(f"{'':-^18s}")
+"""
+
+"""a= 'Hoje tem aula!'
+print(a)
+print(a[0:8:3])"""
+
+"""print(a[-1: -15: -1]) #para escrever de tras pra frente, pq o 1 pula de uma em um para frente, o -1 para tras"""
+
+"""palavra="banana"
+
+print(palavra[::2])""" #para impar, começa pelo 1 e pula de 2 em 2
+
+"""palavra1="banana"
+palavra2="uva"
+
+print(palavra1[1:]+palavra2[1:])
+"""
+
+"""palavra= "banana"
+
+print(palavra[4:6]*3)"""
+
+palavra="arararara"
+
+"""if palavra [::-1] == palavra [0:]:
+    print("ok")"""
+
+#i=0
+#j=0
+#n=0
+# while i<j:
+#     i=i
+#     j=j-1
+#     n=n+1
+
+#i=0
+#j=0
+#n=0
+# while i<10:
+#i=i+1
+#n=n+i+j
+#j=j+1
